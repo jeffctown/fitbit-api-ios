174e904ce91d99e562e020a2b069d977c6e4e81d
Jeff Lett <William.Lett@turner.com>
2017-07-28 20:40:56 -0400

    Adding project files.

---
+++ Fitbit/Fitbit/Fitbit.h
@@ -0,0 +1,19 @@
+//
+//  Fitbit.h
+//  Fitbit
+//
+//  Created by Lett, Jeff on 7/28/17.
+//  Copyright © 2017 Jeff Lett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fitbit.
+FOUNDATION_EXPORT double FitbitVersionNumber;
+
+//! Project version string for Fitbit.
+FOUNDATION_EXPORT const unsigned char FitbitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fitbit/PublicHeader.h>
+
+
